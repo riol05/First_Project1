d6f7ecfe727886f80017cf90037e3395723d20e0
riol05 <dbstlr222@gmail.com>
2024-01-05 21:50:03 +0900

    123

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -26,6 +26,7 @@ public class GameManager : MonoBehaviour
     private void Awake()
     {
         player = playerPrefab.GetComponent<Player>();
+        
         if( Instance != null)
         {
             DestroyImmediate(Instance);
